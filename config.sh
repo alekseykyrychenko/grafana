5455dd010015623fd92eeaa0eab7b6464a9d3d32
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2020-04-09 22:20:59 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,6 +3,11 @@
 echo Start Grafana
 source /etc/sysconfig/grafana-server
 cd /usr/share/grafana
+CONFIG_DIR='/etc/dockerconf/grafana'
+CONFIG_FILE='/etc/grafana/grafana.ini'
+echo ${CONFIG_DIR}/grafana.ini > ${CONFIG_FILE}
+
+sed -i 's//${ROOT_URL}/' ${CONFIG_FILE}
 
 /usr/sbin/grafana-server    --config=${CONF_FILE}                                   \
                             --pidfile=${PID_FILE_DIR}/grafana-server.pid            \
