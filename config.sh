4015ae29906e5f20ff72db842dfe4616811e98bd
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2020-04-09 23:36:57 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -21,10 +21,10 @@ cat ${CONFIG_FILE}
 echo ${LINE}
 
 echo Start Grafana
-/usr/sbin/grafana-server    --config=${CONF_FILE}                                   \
+su -s /bin/bash grafana -c "/usr/sbin/grafana-server    --config=${CONF_FILE}       \
                             --pidfile=${PID_FILE_DIR}/grafana-server.pid            \
                             --packaging=rpm                                         \
                             cfg:default.paths.logs=${LOG_DIR}                       \
                             cfg:default.paths.data=${DATA_DIR}                      \
                             cfg:default.paths.plugins=${PLUGINS_DIR}                \
-                            cfg:default.paths.provisioning=${PROVISIONING_CFG_DIR}
+                            cfg:default.paths.provisioning=${PROVISIONING_CFG_DIR}"
