2de2f442ba7088d1de02d8ce9b353b96ba830675
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2020-04-09 22:35:38 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -5,6 +5,7 @@ source /etc/sysconfig/grafana-server
 cd /usr/share/grafana
 CONFIG_DIR='/etc/dockerconf/grafana'
 CONFIG_FILE='/etc/grafana/grafana.ini'
+LINE='---------------------------'
 cat ${CONFIG_DIR}/grafana.ini > ${CONFIG_FILE}
 
 if [[ ${ROOT_URL} == 'null' ]];
@@ -14,9 +15,10 @@ if [[ ${ROOT_URL} == 'null' ]];
      exit
    fi
 sed -i 's/ROOT_URL/${ROOT_URL}/' ${CONFIG_FILE}
-echo ''
+echo ${LINE}
 echo 'cat '${CONFIG_FILE}
 cat ${CONFIG_FILE}
+echo ${LINE}
 
 echo Start Grafana
 /usr/sbin/grafana-server    --config=${CONF_FILE}                                   \
