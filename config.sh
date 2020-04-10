fb60f44fa14b2f718dfc6c52a1d23afaa5fc222d
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2020-04-10 23:43:44 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -5,9 +5,10 @@ source /etc/sysconfig/grafana-server
 cd /usr/share/grafana
 CONFIG_DIR='/etc/dockerconf/grafana'
 CONFIG_FILE='/etc/grafana/grafana.ini'
-CONFIG_DB='/etc/grafana/'
+DB_FILE='/var/lib/grafana/grafana.db'
 SLEEP='20'
 LINE='---------------------------'
+
 GIT_CONFIG=${CONFIG_DIR}/grafana.ini
 if [[ -f ${GIT_CONFIG} ]];
   then
@@ -17,6 +18,19 @@ if [[ -f ${GIT_CONFIG} ]];
    sleep ${SLEEP}
    exit
   fi
+
+CONFIG_DB=${CONFIG_DIR}/enc.db
+if [[ -f ${CONFIG_DB} ];
+  do
+    rm -f ${DB_FILE}
+    openssl enc -aes-256-cbc -d -a -in ${CONFIG_DB} -out ${DB_FILE} -k ${CRUPT_PASS}
+    du -sh ${DB_FILE}
+    CRUPT_PASS=''
+  else
+    echo 'No DB file '${CONFIG_DB}
+    sleep ${SLEEP}
+    exit
+  done
 
 if [[ ${ROOT_URL} == 'null' ]];
    then
