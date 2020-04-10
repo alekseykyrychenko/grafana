8bec8f314345a01a1529c3f0b9e22c1445a9a7a1
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2020-04-11 00:17:26 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -27,7 +27,7 @@ if [[ -f ${CONFIG_DB} ]];
   then
     rm -f ${DB_FILE}
     openssl enc -aes-256-cbc -d -a -in ${CONFIG_DB} -out ${DB_FILE} -k ${CRUPT_PASS} || exit
-    du -sh ${DB_FILE}
+    chown grafana.grafana ${DB_FILE} 
     rm -f ${CONFIG_DB} 
     CRUPT_PASS=''
   else
