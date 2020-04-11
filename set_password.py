19a5fd946bdf3a93b135bee35d51f5d09af4541d
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2020-04-12 00:48:13 +0300

    Update set_password.py

--- set_password.py
+++ set_password.py
@@ -9,7 +9,7 @@ grafana_api = GrafanaFace(auth=('admin', 'HG73gwe9e3rhel02herq0303h'),
                           host='localhost', port='3000', protocol='http')
 
 grafana_user_id=str(sys.argv[1])
-grafana_pass=str(sys.argv[1])
+grafana_pass=str(sys.argv[2])
 
 print("Set password")
 grafana_api.admin.change_user_password(grafana_user_id, grafana_pass)
