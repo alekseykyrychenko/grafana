2e7d983fe6250153b2791551168ecbf498f6cb27
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2020-04-11 22:14:20 +0300

    Update json.sh

--- json.sh
+++ json.sh
@@ -3,7 +3,7 @@
 echo 'Start "'${1}'"'
 
 su -s /bin/bash grafana -c "${1}" > /dev/null &&\
-      while [[ -z $(curl -Is http://localhost:3000/logind|head -n 1|grep ' 200 OK') ]]; 
+      while [[ -z $(curl -Is http://localhost:3000/login|head -n 1|grep ' 200 OK') ]]; 
         do 
            echo -n -; sleep 1; 
         done && echo 'Start create-dashboard.py' &&\
