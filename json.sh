66b588a73425057082c26916739e6a322a5eed55
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2020-04-11 22:11:44 +0300

    Update json.sh

--- json.sh
+++ json.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-echo 'Import dashboard '${JSON}
+echo 'Start "'${1}'"'
 
-su -s /bin/bash grafana -c "/usr/sbin/grafana-server    --config=${CONF_FILE}                     \
-                            --pidfile=${PID_FILE_DIR}/grafana-server.pid                          \
-                            --packaging=rpm                                                       \
-                            cfg:default.paths.logs=${LOG_DIR}                                     \
-                            cfg:default.paths.data=${DATA_DIR}                                    \
-                            cfg:default.paths.plugins=${PLUGINS_DIR}                              \
-                            cfg:default.paths.provisioning=${PROVISIONING_CFG_DIR}" > /dev/null &&\
+su -s /bin/bash grafana -c "${1}" > /dev/null &&\
       while [[ -z $(curl -Is http://localhost:3000/logind|head -n 1|grep ' 200 OK') ]]; 
         do 
            echo -n -; sleep 1; 
