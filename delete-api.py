2c2a84e9af55cf0220b3374ba7f1ead68d603d94
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2020-04-12 00:29:37 +0300

    Update delete-api.py

--- delete-api.py
+++ delete-api.py
@@ -8,9 +8,8 @@ import glob
 grafana_api = GrafanaFace(auth=('admin', 'HG73gwe9e3rhel02herq0303h'),
                           host='localhost', port='3000', protocol='http')
 
-admin_pass="sdfdfsdf34r5wr35th"
+grafana_user_id="1"
 grafana_pass="sdfdfsdf34r5wr35th"
 
 print("Set password")
-grafana_api.admin.change_user_password("1", admin_pass)
-grafana_api.admin.change_user_password("2", grafana_pass)
+grafana_api.admin.change_user_password(grafana_user_id, admin_pass)
