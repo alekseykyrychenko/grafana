a8995d54184d310ac7d5e082e1806b4d25c566bf
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2020-04-11 20:09:54 +0300

    Update create-dashboard.py

--- create-dashboard.py
+++ create-dashboard.py
@@ -5,7 +5,7 @@ from grafana_api.grafana_face import GrafanaFace
 import json
 
 grafana_api = GrafanaFace(auth='eyJrIjoibEhEUUNFMlJUUzNqRFVlc3lSWWloTExrQ0x3SEEzNTgiLCJuIjoiYXBpIiwiaWQiOjF9',
-                          host='localhost', port='3000', protocol='http', url_path_prefix='monitoring/')
+                          host='localhost', port='3000', protocol='http', url_path_prefix='templet/')
 
 with open('/etc/conf/config.json') as json_file:
   dashboard = json.load(json_file)
