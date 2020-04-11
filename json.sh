47d635e8729dc27f6b9083a2b7941ef563e791d7
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2020-04-11 22:25:45 +0300

    Update json.sh

--- json.sh
+++ json.sh
@@ -2,11 +2,17 @@
 
 echo 'Start "'${1}'"'
 
-su -s /bin/bash grafana -c "${1}" > /dev/null & &&\
-      while [[ -z $(curl -Is http://localhost:3000/login|head -n 1|grep ' 200 OK') ]]; 
-        do 
-           echo -n -; sleep 1; 
-        done && echo 'Start create-dashboard.py' &&\
-     python /etc/dockerconf/grafana/create-dashboard.py
+su -s /bin/bash grafana -c "${1}" &
+
+VAR=0
+while [[ -z $(curl -Is http://localhost:3000/login|head -n 1|grep ' 200 OK') ]]; 
+     do 
+           echo -n -; 
+           sleep 1;
+           VAR=$((${VAR}+1))
+           if [[ ${VAR} > 20 ]]; then exit; fi
+     done && echo 'Start create-dashboard.py' &&\
+python /etc/dockerconf/grafana/create-dashboard.py
+
 kill $(cat /var/run/grafana/grafana-server.pid)        
                             
