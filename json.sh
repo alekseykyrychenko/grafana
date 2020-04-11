c9e24aaf9edca4ce4d5fafa20d81bf2071f57135
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2020-04-11 21:30:15 +0300

    Update json.sh

--- json.sh
+++ json.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo 'Test json '${JSON}
+echo 'Import dashboard '${JSON}
+
+su -s /bin/bash grafana -c "/usr/sbin/grafana-server    --config=${CONF_FILE}       \
+                            --pidfile=${PID_FILE_DIR}/grafana-server.pid            \
+                            --packaging=rpm                                         \
+                            cfg:default.paths.logs=${LOG_DIR}                       \
+                            cfg:default.paths.data=${DATA_DIR}                      \
+                            cfg:default.paths.plugins=${PLUGINS_DIR}                \
+                            cfg:default.paths.provisioning=${PROVISIONING_CFG_DIR}"
+                            
+                            
