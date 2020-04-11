4c7bb7169ce20c534529bec4d3d88c131beecef9
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2020-04-11 22:43:19 +0300

    Update create-dashboard.py

--- create-dashboard.py
+++ create-dashboard.py
@@ -3,6 +3,7 @@
 
 from grafana_api.grafana_face import GrafanaFace
 import json
+import glob
 
 grafana_api = GrafanaFace(auth='eyJrIjoibEhEUUNFMlJUUzNqRFVlc3lSWWloTExrQ0x3SEEzNTgiLCJuIjoiYXBpIiwiaWQiOjF9',
                           host='localhost', port='3000', protocol='http')
