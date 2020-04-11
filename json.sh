cf820d98d4717d53353d72c2bbeddb509ed43ba1
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2020-04-11 22:01:16 +0300

    Update json.sh

--- json.sh
+++ json.sh
@@ -2,17 +2,17 @@
 
 echo 'Import dashboard '${JSON}
 
-su -s /bin/bash grafana -c "/usr/sbin/grafana-server    --config=${CONF_FILE}       \
-                            --pidfile=${PID_FILE_DIR}/grafana-server.pid            \
-                            --packaging=rpm                                         \
-                            cfg:default.paths.logs=${LOG_DIR}                       \
-                            cfg:default.paths.data=${DATA_DIR}                      \
-                            cfg:default.paths.plugins=${PLUGINS_DIR}                \
-                            cfg:default.paths.provisioning=${PROVISIONING_CFG_DIR}" &&\
+su -s /bin/bash grafana -c "/usr/sbin/grafana-server    --config=${CONF_FILE}                     \
+                            --pidfile=${PID_FILE_DIR}/grafana-server.pid                          \
+                            --packaging=rpm                                                       \
+                            cfg:default.paths.logs=${LOG_DIR}                                     \
+                            cfg:default.paths.data=${DATA_DIR}                                    \
+                            cfg:default.paths.plugins=${PLUGINS_DIR}                              \
+                            cfg:default.paths.provisioning=${PROVISIONING_CFG_DIR}" > /dev/null &&\
       while [[ -z $(curl -Is http://localhost:3000/logind|head -n 1|grep ' 200 OK') ]]; 
         do 
            echo -n -; sleep 1; 
-        done &&\
+        done && echo 'Start create-dashboard.py' &&\
      python /etc/dockerconf/grafana/create-dashboard.py
 kill $(cat /var/run/grafana/grafana-server.pid)        
                             
