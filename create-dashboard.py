60f280bb590b809df52f52c45ae5c9d2c8a7db63
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2020-04-11 19:53:01 +0300

    Update create-dashboard.py

--- create-dashboard.py
+++ create-dashboard.py
@@ -1,1 +1,12 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
 
+from grafana_api.grafana_face import GrafanaFace
+import json
+
+grafana_api = GrafanaFace(auth='_______',
+                          host='localhost', port='3000', protocol='https', url_path_prefix='monitoring/')
+
+with open('./test.txt') as json_file:
+  dashboard = json.load(json_file)
+  print (grafana_api.dashboard.update_dashboard(dashboard))
