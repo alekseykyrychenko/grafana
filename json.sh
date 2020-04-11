004d265f5b56e5a9373d99ae2662936298ccc669
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2020-04-11 22:17:40 +0300

    Update json.sh

--- json.sh
+++ json.sh
@@ -2,7 +2,7 @@
 
 echo 'Start "'${1}'"'
 
-su -s /bin/bash grafana -c "${1}" > /dev/null &&\
+su -s /bin/bash grafana -c "${1}" > /dev/null & &&\
       while [[ -z $(curl -Is http://localhost:3000/login|head -n 1|grep ' 200 OK') ]]; 
         do 
            echo -n -; sleep 1; 
