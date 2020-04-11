191f36be001067415bd539154d11748d82e111ef
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2020-04-11 22:42:08 +0300

    Update create-dashboard.py

--- create-dashboard.py
+++ create-dashboard.py
@@ -7,6 +7,8 @@ import json
 grafana_api = GrafanaFace(auth='eyJrIjoibEhEUUNFMlJUUzNqRFVlc3lSWWloTExrQ0x3SEEzNTgiLCJuIjoiYXBpIiwiaWQiOjF9',
                           host='localhost', port='3000', protocol='http')
 
-with open('/etc/conf/config.json') as json_file:
-  dashboard = json.load(json_file)
-  print (grafana_api.dashboard.update_dashboard(dashboard))
+for ls_file in glob.glob("/etc/conf/*.json"):
+  print(ls_file)
+  with open(ls_file) as json_file:
+    dashboard = json.load(json_file)
+    print (grafana_api.dashboard.update_dashboard(dashboard))
