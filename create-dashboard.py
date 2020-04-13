b563eff75a9d2cc85fca8ddadc2ec1ee51c269e6
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2020-04-13 22:21:39 +0300

    Update create-dashboard.py

--- create-dashboard.py
+++ create-dashboard.py
@@ -12,4 +12,7 @@ for ls_file in glob.glob("/etc/conf/*.json"):
   print(ls_file)
   with open(ls_file) as json_file:
     dashboard = json.load(json_file)
-    print (grafana_api.dashboard.update_dashboard(dashboard))
+    try:
+      print (grafana_api.dashboard.update_dashboard(dashboard))
+    except:
+      print ("File: " + ls_file + "not consistent")
