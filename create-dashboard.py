9c64187019ddc256edb1342bd2561a5e7850c9df
alekseykyrychenko <59009480+alekseykyrychenko@users.noreply.github.com>
2020-04-26 00:37:08 +0300

    Update create-dashboard.py

--- create-dashboard.py
+++ create-dashboard.py
@@ -18,9 +18,6 @@ with open("/etc/conf/system.config") as system_file:
 for ls_file in glob.glob("/etc/conf/*.json"):
   print(ls_file)
   with open(ls_file) as json_file:
-    rep_test = json_file.read()
-    rep_test = rep_test.replace('olltv', system_config["org"])
-    print(rep_test)
     dashboard = json.load(json_file)
     dashboard["id"] = None
     dashboard["uid"] = None
